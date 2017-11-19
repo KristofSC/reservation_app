abf104604d7bcfcff31c3f074b17ae0943e5ca5e
Turai Kristóf <turai.kristof@virgo.hu>
2017-11-19 21:06:23 +0100

    171119

---
+++ src/AppBundle/Factory/PatientFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: turai.kristof
+ * Date: 2017.11.18.
+ * Time: 20:28
+ */
+class PatientFactory
+{
+
+}
---
+++ src/AppBundle/Factory/ReservationFactory.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: turai.kristof
+ * Date: 2017.11.19.
+ * Time: 15:34
+ */
+
+namespace AppBundle\Factory;
+
+
+class ReservationFactory
+{
+
+}
